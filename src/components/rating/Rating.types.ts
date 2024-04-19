a4a1f4cae11ae5eb6d1731522c12d9efae4c5c45
joepexel <joepexel@gmail.com>
2024-04-19 18:32:44 +0300

    remove the rating component

--- src/components/rating/Rating.types.ts
+++
@@ -1,7 +0,0 @@
-export interface RatingProps {
-    testIdPrefix: string;
-    title?: string;
-    theme: "primary" | "secondary";
-    disabled?: boolean;
-    size?: "small" | "medium" | "large";
-  }
--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,1 @@
 export * from './components/input';
-export * from './components/rating';
